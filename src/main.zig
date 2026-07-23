44aa2fc35ce76a437270ab3e45cc5eb63c35daf3
samsit-phew <samsitkarki@gmail.com>
2026-07-23 22:04:07 +0545

    phew-phew-sama

--- src/main.zig
+++ src/main.zig
@@ -3,20 +3,27 @@ const std = @import("std");
 pub fn main(init: std.process.Init) !void {
     const args = try init.minimal.args.toSlice(init.arena.allocator());
 
-    if (args.len != 2) {
-        std.debug.print("usage: caz examplefile1.txt\n", .{});
+    if (args.len != 3) {
+        std.debug.print("usage: caz examplefile1.txt <sizeoffile>\n", .{});
         std.log.err("invalid number of args", .{});
         return;
+    }
+    const filePath = args[1];
+    var assumedSize = try std.fmt.parseInt(u16, args[2], 10);
+
+    if (assumedSize > 15360) {
+        assumedSize = 15360;
+        std.log.info("memory is expensive dwag it ramappoclypse lowering the mem use to 15360bytes", .{});
     }
 
     const cwd = std.Io.Dir.cwd();
 
-    const memBuff = try init.gpa.alloc(u8, 1024);
+    const memBuff = try init.gpa.alloc(u8, assumedSize);
     defer init.gpa.free(memBuff);
 
     const contents = cwd.readFile(
         init.io,
-        args[1],
+        filePath,
         memBuff,
     ) catch |err| {
         std.log.err("{}", .{err});
