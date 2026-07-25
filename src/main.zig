113da8bb757696e3976c2ac69592923666670750
samsit-phew <samsitkarki@gmail.com>
2026-07-25 14:05:34 +0545

    v0.0.0-alt-version

--- src/main.zig
+++ src/main.zig
@@ -30,18 +30,10 @@ pub fn main(init: std.process.Init) !void {
 
     var reader = file.reader(init.io, &memBuff);
     var lines: usize = 0;
-    std.debug.print("|{s}                                                                                           |\n", .{filePath});
+    std.debug.print("|{s:<36}\n|\n", .{filePath});
     while (try reader.interface.takeDelimiter('\n')) |line| {
         lines += 1;
-        if (lines < 10) {
-            std.debug.print("| {d}   |---|  {s}\n", .{ lines, line });
-        } else if (lines >= 10 and lines < 100) {
-            std.debug.print("| {d}  |---|  {s}\n", .{ lines, line });
-        } else if (lines >= 100 and lines < 1000) {
-            std.debug.print("| {d} |---|  {s}\n", .{ lines, line });
-        } else {
-            std.debug.print("| {d}|---|  {s}\n", .{ lines, line });
-        }
+        std.debug.print("| {d:>4} |  {s}\n", .{ lines, line });
     }
     std.debug.print("\n", .{});
     std.log.info(" lines read : {d}\n", .{lines});
